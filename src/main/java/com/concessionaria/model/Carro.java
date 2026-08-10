1de7cfb0d620e94babdb69a7a3307f3ee6f52a6f
Fabizzz <165857418+Fabiz2@users.noreply.github.com>
2026-08-10 14:25:48 -0300

    feat: primeiro commit

---
+++ src/main/java/com/concessionaria/model/Carro.java
@@ -0,0 +1,51 @@
+package com.concessionaria.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Table(name = "Carro")
+
+public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String modelo;
+
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(unique = true)
+    private String placa;
+
+    @Column(nullable = false, unique = true)
+    private String chassi;
+
+    @Column(nullable = false)
+    private Integer kilometragem;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private String dtModelo;
+
+    @Column(nullable = false)
+    private String dtFabricacao;
+
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private Condicao condicao;
+}
---
+++ src/main/java/com/concessionaria/model/Cliente.java
@@ -0,0 +1,31 @@
+package com.concessionaria.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Table(name = "Cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+}
